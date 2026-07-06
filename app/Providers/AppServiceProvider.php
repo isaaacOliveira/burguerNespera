0484cf16da75d3585aca77df4d9a7c134f23d18f
isaaacOliveira <pedrohokaoliveira@gmail.com>
2026-07-06 10:03:00 +0100

    fix:add comando https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,9 +23,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         
- // Força o uso de HTTPS se o site não estiver a rodar em localhost
-    if (config('app.env') === 'production' || isset($_SERVER['HTTP_X_FORWARDED_PROTO'])) {
-        URL::forceScheme('https');
     
     // 1. Resolve o erro de chave longa das migrações (SQLSTATE[42000])
         Schema::defaultStringLength(191);
